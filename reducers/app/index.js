d0b66e031c65b3e3deb26a83824f3a63f4307267
Brent Heftye <bheftye92@gmail.com>
2019-04-17 18:43:09 +0200

    Adapting and documenting reducer.

--- reducers/app/index.js
+++ reducers/app/index.js
@@ -1,16 +1,37 @@
 import Immutable from 'immutable'
+import {
+  FETCH_WEATHER_LOADING, FETCH_WEATHER_SUCCESS, FETCH_WEATHER_ERROR}
+  from '../../actions/index'
+
 const initialState = Immutable.fromJS({
   loading: false,
+  fetching: false,
   cities:[
     {name: 'Stockholm,se'},
     {name: 'Bengaluru,in'},
     {name: 'Nairobi,ke'}
   ],
-  citiesWeather: []
+  citiesWeather: [],
+  error: {}
 })
 
 export default (state = initialState, action) => {
   switch (action.type){
+    case FETCH_WEATHER_LOADING:
+      //Update the state fetching so we can give feedback to user.
+      return state.set('fetching', true)
+    case FETCH_WEATHER_SUCCESS:
+      //Update the array containing the weather information, we are no longer fetching.
+      let citiesWeather = state.get('citiesWeather')
+      citiesWeather.push(action.payload)
+      return state
+        .set('citiesWeather', citiesWeather)
+        .set('fetching', false)
+    case FETCH_WEATHER_ERROR:
+      //Update the error object to feedback the user about the error, we are no longer fetching.
+      return state
+        .set('error', action.payload)
+        .set('fetching', false)
     default:
       return state
   }
