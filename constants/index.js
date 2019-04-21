50d272a4ea7fef63f1db69244c8c207cceb93be2
Brent Heftye <bheftye92@gmail.com>
2019-04-21 23:25:17 +0200

    Added constants file.

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+/**
+ * Object containing constant variables.
+ */
+export default {
+  //API endpoints.
+  urls:{
+    weather: 'https://api.openweathermap.org/data/2.5/weather',
+    forecast: 'https://api.openweathermap.org/data/2.5/forecast/daily'
+  },
+  //API key, not secure to do this way, but will guarantee functionality.
+  api:{
+    key: ''
+  }
+}
